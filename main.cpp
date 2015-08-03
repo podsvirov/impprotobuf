c3078f9cc31f20d828ed356d750358561ff275e6
Konstantin Podsvirov <konstantin@podsvirov.pro>
2015-08-03 12:00:19 +0300

    Using well-known type protos

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 
 #include <iostream>
 
+#include <ctime>
+
 namespace gpb = google::protobuf;
 
 int main()
@@ -9,6 +11,8 @@ int main()
   imp::Message msg;
 
   msg.set_text("Hello from Protocol Buffers!");
+  msg.mutable_time()->set_seconds(time(NULL));
+  msg.mutable_time()->set_nanos(0);
 
   std::cout << "imp::Message {\n" << msg.DebugString() << "}" << std::endl;
 
